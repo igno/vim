517b93d4aeec5ee27e95258889be2da1c245a9a4
Erik Jutemar <erik@jutemar.se>
2014-01-21 21:28:50 +0100

    Added sensible.vim to setup.sh
    Added sensible.vim to the bundle plugins that setup.sh installs.
    Added some additional echo output for the different steps.

--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+echo "Setting up folder structure in ~/.vim/"
 mkdir -p ~/.vim/backup
 mkdir -p ~/.vim/autoload
 mkdir -p ~/.vim/bundle
+
+echo "Installing autoload of pathogen.vim"
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
+echo "Installing bundle plugins"
 if [ ! -d ~/.vim/bundle/ctrlp.vim ] ; then
 	git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp.vim
 fi
@@ -20,13 +24,19 @@ if [ ! -d ~/.vim/bundle/vim-airline.vim ] ; then
 	git clone https://github.com/bling/vim-airline ~/.vim/bundle/vim-airline.vim
 fi
 
+if [ ! -d ~/.vim/bundle/vim-sensible ] ; then
+	git clone git://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
+fi
+
 VIMRC=~/.vimrc
 if [ ! -h $VIMRC ] ; then
-	echo "no link file"
 	if [  -e $VIMRC ] ; then
-		echo "found vimrc, backup"
-		mv $VIMRC ~/.vimrc_backup_`date +%s`
+		BACKUP_FILE=~/.vimrc_backup_`date +%s`
+		echo "Making backup of old $VIMRC to $BACKUP_FILE"
+		mv $VIMRC $BACKUP_FILE
 	fi
-	echo "do soft link"
+	echo "Symlinking ~/.vim/vimrc to $VIMRC"
 	ln -s ~/.vim/vimrc $VIMRC
 fi
+
+echo "Done"
