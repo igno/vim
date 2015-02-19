9e43db82c7843223b915f2c16bba27f5416e3e4a
Erik Jutemar <erik@jutemar.se>
2015-02-19 23:42:57 +0100

    add vim-go to bundle

--- setup.sh
+++ setup.sh
@@ -44,6 +44,10 @@ if [ ! -d ~/.vim/bundle/editorconfig ] ; then
 	git clone https://github.com/editorconfig/editorconfig-vim ~/.vim/bundle/editorconfig
 fi
 
+if [ ! -d ~/.vim/bundle/vim-go ] ; then
+	git clone https://github.com/fatih/vim-go.git ~/.vim/bundle/vim-go
+fi
+
 VIMRC=~/.vimrc
 if [ ! -h $VIMRC ] ; then
 	if [  -e $VIMRC ] ; then
